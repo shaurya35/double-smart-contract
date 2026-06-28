ce17e705619b0b438d87de52e40fa7dd0ab15cab
Shaurya <shauryajha35@gmail.com>
2026-06-28 19:10:05 +0530

    test: wrote tests for solana-program with .so file

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,25 @@
-use solana_program::{account_info::AccountInfo,
-    entrypoint::{ProgramResult},
+use borsh::{BorshDeserialize, BorshSerialize};
+use solana_program::{
     entrypoint,
-    pubkey::PubKey
-}
+    account_info::{
+        next_account_info, 
+        AccountInfo
+    },
+    entrypoint::ProgramResult,
+    pubkey::Pubkey,
+};
 
 entrypoint!(process_instruction);
 
 #[derive(BorshDeserialize, BorshSerialize)]
 struct OnChainData {
     count: u32,
-};
+}
 
 fn process_instruction(
-    program_id: &PubKey,
+    _program_id: &Pubkey,
     accounts: &[AccountInfo],
-    instruction_data: &[u8],
+    _instruction_data: &[u8],
 ) -> ProgramResult {
     let mut iter = accounts.iter();
     let data_acc = next_account_info(&mut iter)?;
@@ -24,7 +29,7 @@ fn process_instruction(
     //     Err(err) => {return Err(err)}
     // }
 
-    let counter = OnChainData::try_from_slice(&data_acc.data.borrow_mut())?;
+    let mut counter = OnChainData::try_from_slice(&data_acc.data.borrow())?;
 
     if counter.count == 0 {
         counter.count = 1;
@@ -32,8 +37,7 @@ fn process_instruction(
         counter.count = counter.count * 2;
     }
 
-    counter.serialize(&mut *data_acc.data.borrow_mut());
+    counter.serialize(&mut *data_acc.data.borrow_mut())?;
 
     Ok(())
-
 }
